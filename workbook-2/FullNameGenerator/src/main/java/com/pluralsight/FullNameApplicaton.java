80d8d714abb86c0a37efad7a1f0a5b9541629666
Gabe <gabepineda6@gmail.com>
2024-10-02 10:40:15 -0700

    Changed the formatting and got rid of a while loop to check if string is empty since we already had a function doing that. Made it so that the user can enter their full middle name but the result will still only show the middle initial

--- workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplicaton.java
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplicaton.java
@@ -5,41 +5,37 @@ public class FullNameApplicaton {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         String f = ans("Please enter your first name: ");
+        nCheck(f,"Please enter a first name in order to continue with the program. ");
+
         String l = ans("Please enter your last name: ");
+        nCheck(l,"Please enter a last name in order to continue with the program. ");
 
-        nCheck(f,l);
-
-        String m = ans("Please enter your middle initial: ");
+        String m = ans("Please enter your middle name: ");
         String s = ans("Please enter a suffix :");
 
         name(f,l,m,s);
     }
     public static String ans(String message){
         System.out.println(message);
-        String answer = scanner.nextLine().trim();
-        return answer;
+        return scanner.nextLine().trim();
     }
 
     public static void name(String f, String l, String m, String s){
         String fname = "";
-        while(!f.isEmpty() &&!l.isEmpty()){
-            fname += f + " ";
-            if (!m.isEmpty()){
-                fname += m + ". ";
-            }
-            fname += l;
-            if(!s.isEmpty()){
-                fname += ", " + s;
-            }
-            System.out.println(fname);
-            f = "";
-            l = "";
+        fname += f + " ";
+        if (!m.isEmpty()){
+            fname += m.charAt(0) + ". ";
         }
+        fname += l;
+        if(!s.isEmpty()){
+            fname += ", " + s;
+        }
+        System.out.println(fname);
     }
 
-    public static void nCheck(String f, String l){
-        if (l.isEmpty() && f.isEmpty()){
-            System.out.println("Please enter both a First and a Last name to continue.");
+    public static void nCheck(String x, String message){
+        if (x.isEmpty()){
+            System.out.println(message);
             System.exit(0);
         }
     }
