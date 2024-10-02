dc8edee8b3e25f0a5e750d9155b2e8c27b0a6d3b
Gabe <gabepineda6@gmail.com>
2024-10-02 10:01:53 -0700

    Created a method to turn all intputted names into one full name

--- workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplicaton.java
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameApplicaton.java
@@ -7,11 +7,24 @@ public class FullNameApplicaton {
         String f = ans("Please enter your first name: ");
         String l = ans("Please enter your last name: ");
         String m = ans("Please enter your middle initial: ");
+        String s = ans("Please enter a suffix :");
+
+        name(f,l,m,s);
     }
     public static String ans(String message){
         System.out.println(message);
-        String answer = scanner.nextLine();
-        scanner.nextLine();
+        String answer = scanner.nextLine().trim();
         return answer;
+    }
+
+    public static void name(String f, String l, String m, String s){
+         String fname = "";
+
+        if(!f.isEmpty() && !l.isEmpty()){
+            fname += f + " ";
+            fname += l + " ";
+        }
+
+        System.out.println(fname);
     }
 }
