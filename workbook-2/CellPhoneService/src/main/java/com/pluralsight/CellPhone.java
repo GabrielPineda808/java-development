860c3f86e4ad69179e056002a5ebb9eb0bc043e5
Gabe <gabepineda6@gmail.com>
2024-10-04 13:33:32 -0700

    Creating a dial method and display method. We also made it to have one method that creates multiple phones displaying

--- workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -1,18 +1,33 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class CellPhone {
+    static Scanner s = new Scanner(System.in);
     private final int serialNumber;
     private final String model;
     private final String carrier;
     private final String phoneNumber;
     private final String owner;
 
-    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner){
-        this.serialNumber = serialNumber;
-        this.model = model;
-        this.carrier = carrier;
-        this.phoneNumber = phoneNumber;
-        this.owner = owner;
+    public CellPhone(){
+        System.out.println("What is the serial number? ");
+        this.serialNumber = s.nextInt();
+        s.nextLine();
+        System.out.println("What model is the phone? ");
+        this.model = s.nextLine();
+        System.out.println("Who is the carrier? ");
+        this.carrier = s.nextLine();
+        System.out.println("What is the phone number? ");
+        this.phoneNumber = s.nextLine();
+        System.out.println("Who is the owner of the phone?");
+        this.owner = s.nextLine();
+    }
+    public static void display(CellPhone phone) {
+        System.out.println(phone.getSerialNumber());
+        System.out.println(phone.getModel());
+        System.out.println(phone.getCarrier());
+        System.out.println(phone.getPhoneNumber());
+        System.out.println(phone.getOwner());
     }
 
     public int getSerialNumber(){
@@ -33,5 +48,10 @@ public class CellPhone {
 
     public String getOwner() {
         return owner;
+    }
+
+    public void dial(String phoneNumber){
+        System.out.println(this.owner + "'s phone is calling " +phoneNumber);
+
     }
 }
--- workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -5,29 +5,18 @@ import java.util.Scanner;
 public class CellPhoneApplication {
     static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
-        int serialNumber = Integer.parseInt(ans("What is the serial number? "));
-        String model = ans("What model is the phone? ");
-        String carrier= ans("Who is the carrier? ");
-        String phoneNumber= ans("What is the phone number? ");
-        String owner = ans("Who is the owner of the phone?");
+        CellPhone phone = new CellPhone();
+        CellPhone phone2 = new CellPhone();
 
-        CellPhone phone = new CellPhone(serialNumber,model,carrier,phoneNumber,owner);
-
-        System.out.println(phone.getSerialNumber());
-        System.out.println(phone.getModel());
-        System.out.println(phone.getCarrier());
-        System.out.println(phone.getPhoneNumber());
-        System.out.println(phone.getOwner());
-        
-
-
-
-
+        CellPhone.display(phone);
+        phone.dial( phone2.getPhoneNumber());
+        phone2.dial(phone.getPhoneNumber());
     }
 
     public static String ans(String m){
         System.out.println(m);
         return s.nextLine().trim();
     }
+
 
 }
