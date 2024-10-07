01a8371552fa70bc854b872cfafd20e64632f864
Gabe <gabepineda6@gmail.com>
2024-10-07 15:20:58 -0700

    Created a dowhile loop

---
+++ workbook-2/LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int c = 0;
+        do{
+            System.out.println("I love java");
+            c++;
+        }while(c < 5);
+    }
+}
