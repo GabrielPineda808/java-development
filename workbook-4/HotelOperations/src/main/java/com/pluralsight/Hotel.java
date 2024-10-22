567f95cc65c926aa2b72f13632c60d09b25a3b76
Gabe <gabepineda6@gmail.com>
2024-10-22 14:04:46 -0700

    Created a hotel class that keeps track of the name of the of the user along with how many rooms are vailable/booked, Created a method that will allow the user to book a room if the room is available

--- workbook-4/HotelOperations/src/main/java/com/pluralsight/Hotel.java
+++ workbook-4/HotelOperations/src/main/java/com/pluralsight/Hotel.java
@@ -1,4 +1,85 @@
 package com.pluralsight;
 
 public class Hotel {
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public void setNumberOfSuites(int numberOfSuites) {
+        this.numberOfSuites = numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public void setBookedSuites(int bookedSuites) {
+        this.bookedSuites = bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
+
+    public void setBookedBasicRooms(int bookedBasicRooms) {
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite){
+        if(isSuite && numberOfRooms < this.numberOfSuites){
+            this.bookedSuites += numberOfRooms;
+            this.numberOfSuites -= numberOfRooms;
+                return true;
+        } else if (numberOfRooms < this.numberOfRooms) {
+            this.bookedBasicRooms += numberOfRooms;
+            this.numberOfRooms -= numberOfRooms;
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
+
+
+
 }
+
