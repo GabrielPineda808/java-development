992a83dd6d9bf994c553738bdd6df2d790ddabe0
Gabe <gabepineda6@gmail.com>
2024-10-29 11:35:47 -0700

    Adding classwork

---
+++ workbook-4/StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class NameFormatter {
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ workbook-5/Vehicles/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+
+    {
+        Moped slowRide = new Moped();
+        slowRide.setColor("Red");
+        slowRide.setFuelCapacity(5);
+    }
+}
